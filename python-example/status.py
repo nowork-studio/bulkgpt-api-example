f78bc678e753693b137a80d4b468d3a2934ac96b
Yuting Zhong <izhongyuting@gmail.com>
2024-09-11 00:01:52 -0700

    Add python example

---
+++ python-example/status.py
@@ -0,0 +1,38 @@
+import requests
+
+url = "https://api.bulkgpt.ai/status"
+
+payload = {
+    "taskId": "MTcyNjAzNjU5M18yMDI0LTA5LTExLTA2OjM2OjMz"
+}
+
+headers = {
+    "x-api-key": "<Your BulkGPT API Key>",
+    "Content-Type": "application/json"
+}
+
+response = requests.post(url, json=payload, headers=headers)
+
+print(response.status_code)
+# output: 200
+
+print(response.json())
+"""
+Output Example:
+{
+    'status': {
+        'status': 'completed', 
+        'numOfTasksCompleted': 24, 
+        'numOfTaskSuccess': 24, 
+        'totalNumOfTasks': 24, 
+        'downloadUrl': 'https://example-bulkgpt-download-url.com"
+    }
+}
+
+Explaination:
+    status.status: "preparing" | "inProgress" | "finalizing" | "completed" | "error" | "canceled"
+    status.totalNumOfTasks: Optional, representing the total number of tasks
+    status.numOfTaskSuccess: Optional, representing total number of tasks succeeded
+    status.numOfTasksCompleted: Optional, representing total number of tasks succeeded and failed
+    status.downloadUrl: Optional, only available when status value is "completed", representing the output.csv download url
+"""
---
+++ python-example/upload-trigger.py
@@ -0,0 +1,29 @@
+import base64
+import requests
+
+# Use encode_csv_to_base64 function to encode your csv to base64 string
+def encode_csv_to_base64(file_path):
+    with open(file_path, "rb") as file:
+        csv_content = file.read()
+    encoded_content = base64.b64encode(csv_content).decode('utf-8')
+    return encoded_content
+
+url = "https://api.bulkgpt.ai/upload-trigger"
+
+payload = {
+    "workflowId": "v0#Workflow#6b056f598d994d1ca98821cca221d07e#1721762149339", 
+    "csvBase64": encode_csv_to_base64(file_path="./input.csv")
+}
+
+headers = {
+    "x-api-key": "<Your BulkGPT API Key>",
+    "Content-Type": "application/json"
+}
+
+response = requests.post(url, json=payload, headers=headers)
+
+print(response.status_code)
+# output: 200
+
+print(response.json())
+# output: {'taskId': 'MTcyNjAzNjU5M18yMDI0LTA5LTExLTA2OjM2OjMz'}
